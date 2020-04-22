655f06341af6cda011e840adcda55c7612ebd999
goon39 <59801195+goon39@users.noreply.github.com>
2020-04-22 12:15:58 -0400

    Class setup

---
+++ Ball.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 16:53:08 2020
+
+@author: reill
+"""
+
+import pygame
+import random
+import math
+
+
+WHITE = (255, 255, 255)
+
+
+class Ball(object):
+
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.dx = 0
+        self.dy = 0
+
+
+    def draw(self):
+        return None
+
+
+    def update(self, dt):
+        return None
+        
+
+    def collide_check(self, paddle):
+        return None
+
+
+    def bound(self, screen_height):
+        if self.y < 0:
+            self.y = 0
+            # TODO: math for bouncing off walls
+        if self.y + self.h > screen_height:
+            self.y = screen_height - self.h
+            # TODO: math for bouncing off wall
+
+
+    def score(self, screen_width, paddle1, paddle2):
+        if self.x <= 0:
+            paddle1.score_update()
+            return True
+        elif self.x + self.w >= screen_width:
+            paddle2.score_update()
+            return True
+        else:
+            return False
+
+
+    def serve(self, direction):
+        # serve ball at random angle
+        return None
---
+++ Paddle.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 16:42:15 2020
+
+@author: reill
+"""
+
+import pygame
+
+WHITE = (255, 255, 255)
+
+
+class Paddle(object):
+
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.dy = 0
+        self.dt = 0
+        self.score = 0
+
+
+    def draw(self):
+        return None
+
+
+    def update(self, dt):
+        return None
+
+
+    def score_update(self):
+        self.score += 1
+
+
+    def bound(self, screen_height):
+        if self.y < 0:
+            self.y = 0
+        if self.y + self.h > screen_height:
+            self.y = screen_height - self.h
