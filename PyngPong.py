136439aab1cbf04cf1cc4337980c24506e298cca
goon39 <59801195+goon39@users.noreply.github.com>
2020-04-28 17:43:00 -0400

    Menus

--- PyngPong.py
+++ PyngPong.py
@@ -15,6 +15,7 @@ from Ball import Ball
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (57, 255, 20)
 
 pygame.init()
 pygame.font.init()
@@ -28,6 +29,14 @@ SH = SCREEN_HEIGHT / 2
 type_font = pygame.font.SysFont(None, 48)
 score_font = pygame.font.SysFont(None, 120)
 over_font = pygame.font.SysFont(None, 200)
+title_font = pygame.font.SysFont(None, 160)
+
+#Option menu inputs
+options = {'Color': ['White', 'Green'], 
+           'Difficulty': ['Easy', 'Normal', 'Hard'],
+           }
+inputs = {'Color': None,
+          'Difficulty': None}
 
 #TODO: Add sound during movement
 pygame.mixer.init()
@@ -44,7 +53,119 @@ def text_objects(text, font_obj, color, antialias=True):
     return textSurf, textSurf.get_rect()
 
 
-def main():
+def button(msg, x, y, w, h, ic, ac, fs, itc=WHITE, atc=BLACK, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    text = pygame.font.SysFont("Courier", fs, bold=False)
+    if x + w  > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(screen, ac, (x, y, w, h))
+        textSurf, textRect = text_objects(msg, text, atc)
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(screen, ic, (x, y, w, h))
+        textSurf, textRect = text_objects(msg, text, itc)
+
+    textRect.center = ( (x + (w/2)), (y + (h/2)) )
+    screen.blit(textSurf, textRect)
+
+
+def start_menu():
+
+    start = True
+    while start:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                start = False
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    start = False
+                    pygame.quit()
+                    sys.exit()
+        screen.fill(BLACK)
+        titleSurf, titleRect = text_objects('PONG!', title_font, WHITE)
+        titleRect.centerx = SW
+        titleRect.centery = SH
+        screen.blit(titleSurf, titleRect)
+
+        button('Play', SW, SH + titleRect.getheight()*2, 150, 50, BLACK, WHITE, 48, action=game_loop)
+        button('Options', SW - (150/2), SH + 200, 150, 50, BLACK, WHITE, 48, action=option_menu)
+        pygame.display.update()
+        clock.tick(15)
+
+
+def option_menu():
+
+    menu = True
+    while menu:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu = False
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    menu = False
+                    pygame.quit()
+                    sys.exit()
+        screen.fill(BLACK)
+        titleSurf, titleRect = text_objects('Options', title_font, WHITE)
+        titleRect.centerx = SW
+        titleRect.centery = titleRect.h / 2
+        screen.blit(titleSurf, titleRect)
+
+        optSurf, optRect = text_objects('Color', title_font, WHITE)
+        optRect.w = 2 * SW / (len(options['Color']) + 1)
+        optRect.centerx = 10
+        optRect.centery = titleRect.bottom_left + 10
+        screen.blit(optSurf, optRect)
+
+        if inputs['Color'] == 'Green':
+            button('Green', optRect.x + optRect.w + 10, optRect.y, optRect.w, optRect.h, BLACK, WHITE, 48, itc=GREEN, atc=GREEN)
+        else:
+            # msg, x, y, w, h, ic, ac, fs, itc=WHITE, atc=BLACK, action=None
+            button('White', optRect.x + optRect.w + 10, optRect.y, optRect.w, optRect.h, BLACK, WHITE, 48)
+
+        optSurf, optRect = text_objects('Difficulty', title_font, WHITE)
+        optRect.w = 2 * SW / (len(options['Difficulty']) + 1)
+        optRect.centerx = 10
+        optRect.centery = titleRect.bottom_left + 10
+        screen.blit(optSurf, optRect)
+
+        if inputs['Difficulty'] == 'Easy':
+            button('Green', optRect.x + optRect.w + 10, optRect.y, optRect.w, optRect.h, BLACK, WHITE, 48, itc=GREEN, atc=GREEN)
+        else:
+            # msg, x, y, w, h, ic, ac, fs, itc=WHITE, atc=BLACK, action=None
+            button('White', optRect.x + optRect.w + 10, optRect.y, optRect.w, optRect.h, BLACK, WHITE, 48)
+        
+
+        for opt, o in enumerate(['Color', 'Difficulty']):
+            optSurf, optRect = text_objects(opt, title_font, WHITE)
+            optRect.w = 2 * SW / (len(options[opt]) + 1)
+            optRect.centerx = 10
+            optRect.centery = (o + 1) * (titleRect.bottom_left) + 10
+            screen.blit(optSurf, optRect)
+            for value, v in enumerate(options[opt]):
+                if value != 'Green':
+                    valueSurf, valueRect = text_objects(value, title_font, WHITE)
+                    button(value, (v + 1) * optRect.w + 10, optRect.y, optRect.w, optRect.h, BLACK, WHITE
+                else:
+                    valueSurf, valueRect = text_objects(value, title_font, GREEN)
+                valueRect.w = optRect.w
+                valueRect.centerx = (v + 1) * optRect.w + 10
+                valueRect.centery = optRect.centery
+                screen.blit(valueSurf, valueRect)
+                
+       # button('Play', SW, SH + titleRect.h * 2, 150, 50, BLACK, WHITE, 48, action=game_loop)
+      #  button('Options', SW - (150/2), SH + 200, 150, 50, BLACK, WHITE, 48, action=option_menu)
+        pygame.display.update()
+        clock.tick(15)    
+
+
+def game_loop():
 
     player = Paddle(20, SH - 18, 6, 36)
     computer = Paddle(SCREEN_WIDTH - 20, SH - 18, 6, 36)
